bc73417fce6c46e50f5a1c21fdf353ce13795ab3
Tom Wilson <tom@jackhq.com>
2013-01-19 13:36:32 -0500

    refactored upload and download methods

--- server.js
+++ server.js
@@ -3,15 +3,18 @@ var app = express();
 var config = { redis: { host: '127.0.0.1', port: 6379, options: null}};
 var acct = require('./acct')(config);
 var dfile = require('./file')(config);
+var fs = require('fs');
 
 var knox = require('knox');
 
+app.use(express.favicon());
 app.use(express.bodyParser({
   keepExtensions: true
 }));
+app.use(express.logger('dev'));
 
 app.get('/', function(req,res) {
-  res.send('I should list accounts');
+  res.send("Welcome to fscat\n--------------------\nhttps://github.com/twilson63/fscat");
 });
 
 app.get('/:acct', function(req, res) {
@@ -39,44 +42,58 @@ app.post('/:acct/upload', function(req,res) {
   var file = req.files[Object.keys(req.files)[0]];
   var filePath = file.path;
   var fileName = file.name.split(' ').join('_');
+  var key = req.params.acct + ':' + fileName;
+  var s3 = filePath.split('/').pop();
+
+  var deleteTempFile = function() {
+    fs.unlink(filePath, function (err) {
+      if (err) { return console.log(err) };
+      console.log('successfully deleted ' + filePath);
+    });    
+  }
+
+  var saveFileData = function(err, res) {
+    if(err) { return console.log(err) };
+    dfile.set( key, { s3: s3 }, function(err) {
+      if (err) { return console.log(err); }
+      // remove temp file
+      deleteTempFile();
+    });
+  };
+
   res.send(fileName);
   acct.get(req.params.acct, function(doc) {
     doc.bucket = 'fscat_' + req.params.acct;
     var client = knox.createClient(doc);
-    client.putFile(filePath, filePath.split('/').pop(), function(err, res) {
-      if(err) { return console.log(err) };
-      dfile.set(req.params.acct + ':' + fileName, 
-        { s3: filePath.split('/').pop() },
-        function(err) {
-          console.log('Saved to Redis!');
-        });
-    });
+    client.putFile(filePath, s3, saveFileData);
   });
 });
 
 app.get('/:acct/:name', function(req, res){
+  var streamResponse = function(err, s3Object) {
+    if (err) { res.send('ERROR'); }
+    res.writeHead(200, s3Object.headers);
+    s3Object.on('data', function(chunk) {
+      res.write(chunk);
+    });
+    s3Object.on('end', function() {
+      res.end();
+    });
+  }
   // stream file to response
   acct.get(req.params.acct, function(doc) {
     doc.bucket = 'fscat_' + req.params.acct;
     var client = knox.createClient(doc);
     dfile.get(req.params.acct + ':' + req.params.name, function(doc){
-      client.getFile(doc.s3, function(err, result) {
-        res.writeHead(200, result.headers);
-        result.on('data', function(chunk) {
-          res.write(chunk);
-        });
-        result.on('end', function() {
-          res.end;
-        });
-      });
+      if (!doc) { return res.send('File Not Found'); }
+      client.getFile(doc.s3, streamResponse);
     });
   });
 });
 
-app.del('/:acct/:name', function(req, res){
-  // get account
-  // delete file
-});
-
+// app.del('/:acct/:name', function(req, res){
+//   // get account
+//   // delete file
+// });
 
 app.listen(3000);
